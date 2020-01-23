23e4e5b44a80f1175a3a5402515885c611997303
Marina8989 <marina.mill209@gmail.com>
2020-01-22 17:09:14 -0800

    added DOM to navigation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+//navigation
+
+let logoHeading = document.querySelector(".logo-heading");
+logoHeading.addEventListener("mouseenter", () => {
+    logoHeading.style.color = "orange";
+
+    logoHeading.addEventListener("mouseleave", () => {
+        logoHeading.style.color = "black";
+    })
+});
